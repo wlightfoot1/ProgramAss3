c7503ba77b0d6960b531ec09438db385970c856a
wlightfoot1 <36997986+wlightfoot1@users.noreply.github.com>
2018-04-06 12:22:26 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,16 +5,26 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+/*
+ * Westin Lightfoot
+ * Program 3
+ * 
+ * In the input text file I ignored the first line, (A,B,C,D,E,F)
+ * Used "*" for the INF in my input file
+ * 
+ * 
+ */
+
 public class Main {
-	static String[][] matrix = new String[7][6];
-	static int[][] matrix2 = new int[6][6];
-	static char[] key = {'A', 'B', 'C', 'D', 'E', 'F'};
+	static int[][] matrix = new int[6][6];
+	
+	//static char[] key = {'A', 'B', 'C', 'D', 'E', 'F'};
 	public static void print(){ //prints 2D Array Matrix
 		System.out.println("Matrix\n" + "-----------");
 		System.out.println("A B C D E F");
-		for(int row = 0; row < matrix2.length; row++){
-			for(int col = 0; col < matrix2[row].length; col++){
-				System.out.print(matrix2[row][col] + " ");
+		for(int row = 0; row < matrix.length; row++){
+			for(int col = 0; col < matrix[row].length; col++){
+				System.out.print(matrix[row][col] + " ");
 			}
 			System.out.println();
 		}
@@ -27,13 +37,17 @@ public class Main {
 			String line = null;
 			System.out.println("**File Found**\n");
 			String[] data;
-			for(int row = 0; row < matrix2.length; row++){			//Storing file matrix to a 2D Array
+			for(int row = 0; row < matrix.length; row++){			//Storing file matrix to a 2D Array
 				line = readInput.readLine();
 				data = line.split(",");
-				
-				for(int col = 0; col < matrix2[row].length; col++){
-					int val = Integer.parseInt(data[col]);
-					matrix2[row][col] = val;
+				for(int col = 0; col < matrix[row].length; col++){
+					if(data[col].equals("*")){
+						int INF = Integer.MAX_VALUE;
+						matrix[row][col] = INF;
+					} else{
+						int val = Integer.parseInt(data[col]);
+						matrix[row][col] = val;
+					}
 				}
 			}
 			readInput.close();	
